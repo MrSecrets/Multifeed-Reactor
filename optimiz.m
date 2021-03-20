3c9d53bb273feae888412f3bdd27e1b19e3c5a10
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2021-03-21 01:11:51 +0530

    optimization left

--- optimiz.m
+++ optimiz.m
@@ -9,29 +9,16 @@ phi=0.63;
 Dp=0.001;
 mu=2.31*10^(-5);
 ro_o=21.9;
-L=6;
-bed = 3;
+k=1.034/36960320250;
 
 
 D= 0.5;
-Ac=3.14*D*D/4;
+Ac=pi*D*D/4;
 u=vo/Ac;
 % u = 1.1210;
 G=ro_o*u;
 
 beta=G*(1-phi)/(ro_o*Dp*phi^3)*(150*(1-phi)*mu/Dp + 1.75*G);
-
-Fao=70.13;
-k=1.034/36960320250;
-% w=Ac*ro_b*z;
-x_0=0;
-
-
-Fa0 = 70.13; %meoh
-Fb0= 70.87; %hcl
-Fc0= 0;    %mecl
-Fd0= 0;     %steam
-theta = [Fa0/Fa0; Fb0/Fa0; Fc0/Fa0; Fd0/Fa0;];
 
 cp_meoh=64;
 cp_hcl=29.5;
@@ -44,63 +31,114 @@ delCp=cp_steam+cp_mecl-cp_hcl-cp_meoh;
 
 thermSystem = [delH_298; delCp];
 
-convConst = Ac*ro_b*k/Fao;
 T0 = 573.15;
+Tmeoh = 273.15+50;
+Tout = T0;
 
+global Tvalues
+Tvalues= [];
 
-zspan = [0 2];
-y0 = [P0; 0; T0];
-% [z,y]=ode45(@(z, y)ReactorODE,zspan, y0, P0);
-[z, y]=ode45(@(z, y)ReactorODE(z,y,P0, T0, beta, convConst, theta, cpData, thermSystem),zspan,y0);
+Fa=70.13;
+Fa0 = 0;
+Fb0= 70.87; %hcl
+Fc0= 0;    %mecl
+Fd0= 0;     %steam
+feedIn = [0.5 0.5];
 
-% P = y(:,1)
-figure(1)
-plot (z,y(:,1)/101325);
-xlabel('z');
-ylabel('Pressure')
-legend('P');
+feedIn = feedIn/sum(feedIn)
 
-figure(2)
-plot (z,y(:,2));
-xlabel('z');
-ylabel('Conversion');
-legend('x');
+for i = 1:length(feedIn)
+    
+    
+    Fa0 = Fa0 + Fa*feedIn(i); %meoh
+    V = (Fa0+Fb0+Fc0+Fb0)*8.314*T0/P0;
 
-figure(3)
-plot (z,y(:,3));
-xlabel('z');
-ylabel('Temperature');
-legend('T');
+    theta = [Fa0/Fa0; Fb0/Fa0; Fc0/Fa0; Fd0/Fa0;];
+    
+    convConst = Ac*ro_b*k/Fa0;
+    
+    Tin = (theta(1)*cpData(1)*Tmeoh + theta(2)*cpData(2)*Tout + theta(3)*cpData(3)*Tout + theta(4)*cpData(4)*Tout) ...
+        /(theta(1)*cpData(1)+theta(2)*cpData(2)+theta(3)*cpData(3)+theta(4)*cpData(4));
+    
+    zspan = [0 2];
+    y0 = [P0; 0];
+    
+    if i == 1
+        Tin = T0;
+    end
+    
+    [z, y]=ode45(@(z, y)ReactorODE(z,y,P0, Tin, beta, convConst, theta, cpData, thermSystem),zspan,y0);
+    
+    P = y(:,1);
+    x = y(:,2);
 
+    figure(10*i+1)
+    plot (z,y(:,1)/101325);
+    xlabel('z');
+    ylabel('Pressure')
+    legend('P');
 
-fprintf('P = %16.6f \n',y(length(y),1)/ 101325);
-fprintf('x = %16.6f \n',y(length(y),2));
-fprintf('T = %16.6f \n',y(length(y),3));
+    figure(10*i+2)
+    plot (z,y(:,2));
+    xlabel('z');
+    ylabel('Conversion');
+    legend('x');
 
+    termin = find(x>0.99);
 
+    if isempty(termin)
 
+      x_t = x(end);
+      P0 = P(end);
+      l = z(end);
+      Tout = Tvalues(end);
 
+    else          
+      indi = termin(1);
+      x_t = x(indi);
+      P0 = P(indi);
+      l = z(indi);
+      Tout = Tvalues(indi);
+    end
+
+    Fb0 = Fb0 - x_t*Fa0;
+    Fc0 = Fc0 + x_t*Fa0;
+    Fd0 = Fc0 + x_t*Fa0;
+    Fa0 = Fa0 - x_t*Fa0;
+    
+    Ac = V/u;
+    D = sqrt(4*Ac/pi);
+    
+    fprintf('P = %16.6f \n',P0/ 101325);
+    fprintf('x = %16.6f \n',x_t);
+    fprintf('z = %16.6f \n',l);
+    fprintf('D = %16.6f \n',D);
+    fprintf('Tout = %16.6f \n',Tout);
+    fprintf('Tin = %16.6f \n',Tin);
+    
+    Tvalues = [];
+    
+end
 
 function Reactor_diff = ReactorODE(z,Yfuncvec, P0, T0, beta,convConst, theta, cpData, thermSystem)
    P = Yfuncvec(1);
    x = Yfuncvec(2);
-   T = Yfuncvec(3);
-
+%    T = Yfuncvec(3);
+   global Tvalues 
    
    dPdz = 0 - beta*(P0/P);
    dxdz = convConst*(P/2.01)^(2)*((theta(2)-x))*((theta(1)-x));
-   dTdz = (x* -(thermSystem(1)+thermSystem(2)*(T-298)) ...
+   dTdz = (-x*thermSystem(1) ...
         + theta(1)*cpData(1)*T0 + theta(2)*cpData(2)*T0 + theta(3)*cpData(3)*T0 + theta(4)*cpData(4)*T0 ...
         + x*thermSystem(2)*298) ...
         /(theta(1)*cpData(1)+theta(2)*cpData(2)+theta(3)*cpData(3)+theta(4)*cpData(4)+x*thermSystem(2));
-%    dTdz = (x* -(thermSystem(1)+thermSystem(2)*(T-298)) ...
-%         + theta(1)*cpData(1)*T0 + theta(2)*cpData(2)*T0 + theta(3)*cpData(3)*T0 + theta(4)*cpData(4)*T0 + x*thermSystem(2)*T0) ...
-%         /(theta(1)*cpData(1) + theta(2)*cpData(2) + theta(3)*cpData(3) + theta(4)*cpData(4) + x*thermSystem(2));
+%    dTdz = ( ...
+%         + theta(1)*cpData(1)*T0 + theta(2)*cpData(2)*T0 + theta(3)*cpData(3)*T0 + theta(4)*cpData(4)*T0) ...
+%         /(theta(1)*cpData(1) + theta(2)*cpData(2) + theta(3)*cpData(3) + theta(4)*cpData(4));
+    Tvalues = [Tvalues, dTdz];
+    
 
-
-
-Reactor_diff = [dPdz; dxdz; dTdz];
+Reactor_diff = [dPdz; dxdz];
 end
-
 
 
