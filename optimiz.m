5014b13319eb1b9d52cd7ab2086e2da2c8ad2329
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2021-03-21 14:52:12 +0530

    added variable k

--- optimiz.m
+++ optimiz.m
@@ -9,10 +9,10 @@ phi=0.63;
 Dp=0.001;
 mu=2.31*10^(-5);
 ro_o=21.9;
-k=1.034/36960320250;
+k_conv=36960320250;
+k_cons = 1.034;
 
-
-D= 0.5;
+D= 0.4;
 Ac=pi*D*D/4;
 u=vo/Ac;
 % u = 1.1210;
@@ -20,6 +20,7 @@ G=ro_o*u;
 
 beta=G*(1-phi)/(ro_o*Dp*phi^3)*(150*(1-phi)*mu/Dp + 1.75*G);
 
+latentMeoh = 400*32.04;
 cp_meoh=64;
 cp_hcl=29.5;
 cp_mecl=81.2;
@@ -32,7 +33,7 @@ delCp=cp_steam+cp_mecl-cp_hcl-cp_meoh;
 thermSystem = [delH_298; delCp];
 
 T0 = 573.15;
-Tmeoh = 273.15+50;
+Tmeoh = 273.15+70;
 Tout = T0;
 
 global Tvalues
@@ -43,7 +44,7 @@ Fa0 = 0;
 Fb0= 70.87; %hcl
 Fc0= 0;    %mecl
 Fd0= 0;     %steam
-feedIn = [0.5 0.5];
+feedIn = [0.3 0.3 0.4];
 
 feedIn = feedIn/sum(feedIn)
 
@@ -55,10 +56,10 @@ for i = 1:length(feedIn)
 
     theta = [Fa0/Fa0; Fb0/Fa0; Fc0/Fa0; Fd0/Fa0;];
     
-    convConst = Ac*ro_b*k/Fa0;
     
-    Tin = (theta(1)*cpData(1)*Tmeoh + theta(2)*cpData(2)*Tout + theta(3)*cpData(3)*Tout + theta(4)*cpData(4)*Tout) ...
+    Tin = (theta(1)*cpData(1)*Tmeoh + theta(2)*cpData(2)*Tout + theta(3)*cpData(3)*Tout + theta(4)*cpData(4)*Tout - latentMeoh*theta(1)) ...
         /(theta(1)*cpData(1)+theta(2)*cpData(2)+theta(3)*cpData(3)+theta(4)*cpData(4));
+   
     
     zspan = [0 2];
     y0 = [P0; 0];
@@ -66,6 +67,9 @@ for i = 1:length(feedIn)
     if i == 1
         Tin = T0;
     end
+   
+    k = KCons(Tin)/k_conv;
+    convConst = Ac*ro_b*k/Fa0;
     
     [z, y]=ode45(@(z, y)ReactorODE(z,y,P0, Tin, beta, convConst, theta, cpData, thermSystem),zspan,y0);
     
@@ -113,12 +117,29 @@ for i = 1:length(feedIn)
     fprintf('x = %16.6f \n',x_t);
     fprintf('z = %16.6f \n',l);
     fprintf('D = %16.6f \n',D);
-    fprintf('Tout = %16.6f \n',Tout);
-    fprintf('Tin = %16.6f \n',Tin);
+    fprintf('Tout = %16.6f \n',Tout-273.15);
+    fprintf('Tin = %16.6f \n',Tin-273.15);
     
     Tvalues = [];
     
 end
+
+function k = KCons(T)
+
+    T= T-273.15;
+    
+    if T<330
+        k = 1.034;
+    elseif T<360
+        k = 2.202;
+    elseif T<390
+        k = 3.404;
+    else 
+        k = 7.714;
+    end
+
+end
+
 
 function Reactor_diff = ReactorODE(z,Yfuncvec, P0, T0, beta,convConst, theta, cpData, thermSystem)
    P = Yfuncvec(1);
@@ -128,16 +149,13 @@ function Reactor_diff = ReactorODE(z,Yfuncvec, P0, T0, beta,convConst, theta, cp
    
    dPdz = 0 - beta*(P0/P);
    dxdz = convConst*(P/2.01)^(2)*((theta(2)-x))*((theta(1)-x));
-   dTdz = (-x*thermSystem(1) ...
+   dTdx = (-x*thermSystem(1) ...
         + theta(1)*cpData(1)*T0 + theta(2)*cpData(2)*T0 + theta(3)*cpData(3)*T0 + theta(4)*cpData(4)*T0 ...
         + x*thermSystem(2)*298) ...
         /(theta(1)*cpData(1)+theta(2)*cpData(2)+theta(3)*cpData(3)+theta(4)*cpData(4)+x*thermSystem(2));
-%    dTdz = ( ...
-%         + theta(1)*cpData(1)*T0 + theta(2)*cpData(2)*T0 + theta(3)*cpData(3)*T0 + theta(4)*cpData(4)*T0) ...
-%         /(theta(1)*cpData(1) + theta(2)*cpData(2) + theta(3)*cpData(3) + theta(4)*cpData(4));
-    Tvalues = [Tvalues, dTdz];
     
-
+   Tvalues = [Tvalues, dTdx];
+    
 Reactor_diff = [dPdz; dxdz];
 end
 
